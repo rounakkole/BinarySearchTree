c31b0dafb6f144117cbe64338abd3ef5269a7df6
rounak <rounakkole@gmail.com>
2022-02-28 22:24:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary search tree");
+        }
+    }
+}
